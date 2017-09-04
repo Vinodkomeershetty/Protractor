71e18cd1b859c2c2ae1fb285b67664fee2952d2e
vinodk <komeershettyvinod@gmail.com>
2017-09-04 15:24:08 +0530

    Commit basic config.js

--- config.js
+++ config.js
@@ -1,13 +1,23 @@
 exports.config = {
+
+    //SeleniumAddress field points for selenium-server address
     seleniumAddress: 'http://localhost:4444/wd/hub',
 
+    //This option is used to directly connect to web driver server
+    // directConnect: true,
+
+    //Specifies the environment that you want to run in
     capabilities: {
         'browserName': 'chrome'
     },
 
-    specs: ['example-spec.js'],
+    //Specs field tells protractor where are your tests.
+        specs: ['examplespec.js'],
 
     jasmineNodeOpts: {
-        showColors: true
+
+        // If true  print colors to the terminal.
+        showColors: true,
+        // defaultTimeoutInterval: 2500000
     }
 };
